96dcc9020773b4dfb19f9c9448e0cc083165967b
Cedrick Cooke <development@cedrickc.net>
2023-12-21 14:38:13 -0800

    Durability jsValue as extension val

--- core/src/jsMain/kotlin/Durability.kt
+++ core/src/jsMain/kotlin/Durability.kt
@@ -1,10 +1,19 @@
 package com.juul.indexeddb
 
+import com.juul.indexeddb.Durability.Default
+import com.juul.indexeddb.Durability.Relaxed
+import com.juul.indexeddb.Durability.Strict
+
 /** https://developer.mozilla.org/en-US/docs/Web/API/IDBTransaction/durability */
-public enum class Durability(
-    internal val jsValue: String
-) {
-    Default("default"),
-    Strict("strict"),
-    Relaxed("relaxed");
+public enum class Durability {
+    Default,
+    Strict,
+    Relaxed,
 }
+
+internal val Durability.jsValue: String
+    get() = when (this) {
+        Default -> "default"
+        Strict -> "strict"
+        Relaxed -> "relaxed"
+    }
