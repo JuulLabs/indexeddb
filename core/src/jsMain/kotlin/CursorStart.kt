c8dccd27deabee51c1effaf1fd7b4456bc83c498
Travis Wyatt <travis@juul.com>
2024-07-06 23:43:22 -0700

    `./gradlew formatKotlin`

--- core/src/jsMain/kotlin/CursorStart.kt
+++ core/src/jsMain/kotlin/CursorStart.kt
@@ -6,19 +6,26 @@ public sealed class CursorStart {
 
     internal abstract fun apply(cursor: IDBCursor)
 
-    public data class Advance(val count: Int) : CursorStart() {
+    public data class Advance(
+        val count: Int,
+    ) : CursorStart() {
         override fun apply(cursor: IDBCursor) {
             cursor.advance(count)
         }
     }
 
-    public data class Continue(val key: Key) : CursorStart() {
+    public data class Continue(
+        val key: Key,
+    ) : CursorStart() {
         override fun apply(cursor: IDBCursor) {
             cursor.`continue`(key.toJs())
         }
     }
 
-    public data class ContinuePrimaryKey(val key: Key, val primaryKey: Key) : CursorStart() {
+    public data class ContinuePrimaryKey(
+        val key: Key,
+        val primaryKey: Key,
+    ) : CursorStart() {
         override fun apply(cursor: IDBCursor) {
             cursor.continuePrimaryKey(key.toJs(), primaryKey.toJs())
         }
