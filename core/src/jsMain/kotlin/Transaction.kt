403e93fbe3f16c9ea676163acd5a6e245890c433
Travis Wyatt <travis.i.wyatt@gmail.com>
2024-11-11 10:25:34 -0800

    `./gradlew formatKotlin`

--- core/src/jsMain/kotlin/Transaction.kt
+++ core/src/jsMain/kotlin/Transaction.kt
@@ -8,7 +8,6 @@ import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import org.w3c.dom.events.Event
-import kotlin.js.Json
 
 public open class Transaction internal constructor(
     internal val transaction: IDBTransaction,
