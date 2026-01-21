be9805146bcb0927ab6998ffc14c0213e5a8947c
Cedrick Cooke <development@cedrickc.net>
2026-01-21 13:50:03 -0800

    Fix type for IDBFactory.databases

--- external/src/webMain/kotlin/IDBFactory.kt
+++ external/src/webMain/kotlin/IDBFactory.kt
@@ -2,14 +2,13 @@ package com.juul.indexeddb.external
 
 import kotlin.js.JsAny
 import kotlin.js.JsArray
-import kotlin.js.JsString
 import kotlin.js.Promise
 import kotlin.js.js
 
 /** https://developer.mozilla.org/en-US/docs/Web/API/IDBFactory */
 public external class IDBFactory : JsAny {
     public fun open(name: String, version: Int): IDBOpenDBRequest
-    public fun databases(): Promise<JsArray<JsString>>
+    public fun databases(): Promise<JsArray<IDBAvailableDatabase>>
     public fun deleteDatabase(name: String): IDBOpenDBRequest
 }
 
